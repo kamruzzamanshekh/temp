b871b497943476b95884cce41001236a3c90ef4b
kamruzzaman shekh <shekhnstuiit@gmail.com>
2026-06-10 23:15:39 +0600

    V3.9

--- tests/test_open_website.py
+++ tests/test_open_website.py
@@ -1,13 +1,5 @@
-def test_open_website(driver):
-    url = "https://www.accuweather.com/en/bd/dhaka/28143/weather-forecast/28143"  # Change this to your target website
-    driver.get(url)
-    driver.implicitly_wait(100) 
+import time
 
-def test_page_title_contains_dhaka(driver):
-    url = "https://www.accuweather.com/en/bd/dhaka/28143/weather-forecast/28143"
-    driver.get(url)
-
-    assert "Dhaka" in driver.title, "Page title does not contain Dhaka"
 
 def test_current_temperature_visible(driver):
     url = "https://www.accuweather.com/en/bd/dhaka/28143/weather-forecast/28143"
@@ -15,6 +7,7 @@ def test_current_temperature_visible(driver):
 
     item = driver.find_element("xpath", "/html/body/div/div[3]/div/div[3]/a[7]/span")
     item.click()
+    time.sleep(5)  # Wait for the page to load after clicking
     # Locate the element using XPath
     element = driver.find_element("xpath", '//*[@id="current"]/div/div/div[2]/div[1]/div/div/div/div[1]')
 
